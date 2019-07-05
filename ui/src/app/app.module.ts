b7e50389807e4a1257ee31843512ef0af2ecd07b
Matej Chalk <matej.chalk@flowup.cz>
2019-07-05 12:49:06 +0200

    feat(ui/message-form): create component for sending message

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,10 +8,16 @@ import { MessageComponent } from './components/message/message.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { MessagesService } from './services/http/messages.service';
 import { LikesService } from './services/http/likes.service';
+import { MessageFormComponent } from './components/message-form/message-form.component';
 
 @NgModule({
-  declarations: [AppComponent, MessageComponent, MessagesComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    MessageComponent,
+    MessagesComponent,
+    MessageFormComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [MessagesService, LikesService],
   bootstrap: [AppComponent],
 })
--- ui/src/app/components/messages/messages.component.ts
+++ ui/src/app/components/messages/messages.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { of } from 'rxjs';
 import { MessageWithLikesModel } from '../../models/message-with-likes.model';
+import { LikesService } from '../../services/http/likes.service';
+import { MessagesService } from '../../services/http/messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -9,34 +10,14 @@ import { MessageWithLikesModel } from '../../models/message-with-likes.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessagesComponent {
-  readonly messages$ = of(
-    Array(15)
-      .fill(null)
-      .map(
-        (_, i): MessageWithLikesModel => ({
-          id: Math.random()
-            .toString(26)
-            .substr(2),
-          text: `
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          `.substr(0, Math.random() * 250),
-          timestamp: Date.now(),
-          author: {
-            name: `User #${i + 1}`,
-            color:
-              '#' +
-              Math.random()
-                .toString(16)
-                .substr(2, 6),
-          },
-          likes: Math.round((Math.random() - 0.5) * 10),
-        }),
-      ),
-  );
+  readonly messages$ = this.messagesService.messages$;
+
+  constructor(
+    private readonly messagesService: MessagesService,
+    private readonly likesService: LikesService,
+  ) {}
 
   likeMessage(message: MessageWithLikesModel): void {
-    console.log('Liked message', message);
+    this.likesService.sendLike(message.id);
   }
 }
