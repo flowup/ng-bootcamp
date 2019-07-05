edf2fc50e340c24461be3a3f9a636577b45f11ec
Matej Chalk <matej.chalk@flowup.cz>
2019-07-05 14:39:14 +0200

    feat(ui/emoji): create emoji pipe

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessageFormComponent } from './components/message-form/message-form.component';
 import { MessageComponent } from './components/message/message.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { MessagesService } from './services/http/messages.service';
+import { EmojiPipe } from './pipes/emoji.pipe';
 import { LikesService } from './services/http/likes.service';
-import { MessageFormComponent } from './components/message-form/message-form.component';
+import { MessagesService } from './services/http/messages.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { MessageFormComponent } from './components/message-form/message-form.com
     MessageComponent,
     MessagesComponent,
     MessageFormComponent,
+    EmojiPipe,
   ],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [MessagesService, LikesService],
